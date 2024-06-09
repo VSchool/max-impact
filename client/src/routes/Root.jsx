eb4122dfccecc144225bc89345bb6662a7dac7a8
Clint Baker <clintjbaker@gmail.com>
2024-06-09 14:39:42 -0600

    adjust button styling

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
-import Navbar from '../components/Navbar'
 import '../styles/global.css'
 
 export const Root = ({ admin }) => {
