3c4500c45adb9ee6972c34c3375b96c08819f10d
ifedorov1402 <131213710+ifedorov1402@users.noreply.github.com>
2024-06-25 23:03:21 -0400

    Mobile veiw for Archive and Lessons1

--- client/src/components/AdminPage.jsx
+++ client/src/components/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './AdminPage.css';
 import uploadIcon from '/src/assets/icons/upload.svg';
 import AdminLesson from './AdminLesson';
@@ -81,75 +81,4 @@ export default AdminPage;
 
 
 
-
-// import React from 'react';
-// import './AdminPage.css';
-// import uploadIcon from '/src/assets/icons/upload.svg';
-// import AdminLesson from './AdminLesson';
-// import Sidebar from './Sidebar';
-
-
-
-
-// const lessons = [
-//   {
-//     title: "Class 1",
-//     date: "Uploaded 5/8/2024",
-//     attachment: "Download Attachment",
-//     imgSrc: "/src/assets/icons/video_card.svg",
-//   },
-//   {
-//     title: "Class 1",
-//     date: "Uploaded 5/8/2024",
-//     attachment: "Download Attachment",
-//     imgSrc: "/src/assets/icons/video_card.svg",
-//   },
-//   {
-//     title: "Class 1",
-//     date: "Uploaded 5/8/2024",
-//     attachment: "Download Attachment",
-//     imgSrc: "/src/assets/icons/video_card.svg",
-//   },
-
-// ];
-
-
-// const AdminPage = () => {
-//     return (
-//         <>
-//         <div className="admin-page">
-//         <Sidebar isAdmin={true} isOpen={true} />
-//           <section className="admin-greeting-section">
-//             <h1 className="admin-dashboard-title">Hello, Admin</h1>
-//           </section>
-//           <h2 className="section-title">New Course Upload</h2>
-//           <section className="course-upload-section">
-//             <input className="class-name-input" type="text" placeholder="Class Name" />
-//             <div className="upload-buttons">
-//               <button className="upload-button">
-//                 <img src={uploadIcon} alt="Upload Video" />
-//                 Upload Video
-//               </button>
-//               <button className="upload-button">
-//                 <img src={uploadIcon} alt="Upload PDF" />
-//                 Upload PDF
-//               </button>
-//             </div>
-//             <button className="publish-button">Publish</button>
-//           </section>
-//           <section className="uploaded-courses-section">
-//             <h2 className="section-title">Uploaded Course Videos</h2>
-//             <div className="lessons-container">
-//               {lessons.map((lesson, index) => (
-//                 <AdminLesson key={index} {...lesson} />
-//               ))}
-//             </div>
-//           </section>
-          
-//        </div>
-//        </>
-//     );
-//   };
-  
-//   export default AdminPage;
 
--- client/src/components/LessonsDisplayArea.jsx
+++ client/src/components/LessonsDisplayArea.jsx
@@ -1,5 +1,3 @@
-// LessonsDisplayArea.js
-
 
 // LessonsDisplayArea.js
 import React, { useState } from 'react';
@@ -68,50 +66,3 @@ export default LessonsDisplayArea;
 
 
 
-// import React from 'react';
-// import Lesson from './Lessons';
-// import './LessonsDisplayArea.css';
-// import Sidebar from './Sidebar';
-
-// const lessons = [
-//     {
-//       title: "Class 1",
-//       date: "Uploaded 5/8/2024",
-//       attachment: "Download Attachment",
-//       imgSrc: "/src/assets/icons/video_card.svg",
-//     },
-//     {
-//       title: "Class 2",
-//       date: "Uploaded 5/8/2024",
-//       attachment: "Download Attachment",
-//       imgSrc: "/src/assets/icons/video_card.svg",
-//     },
-//   ];
-  
-//   const LessonsDisplayArea = () => {
-//     return (
-//       <div className="lessons-display-area">
-//         <Sidebar />
-//         <section className="user-greeting-section">
-//           <h1 className="dashboard-title">Hello, [User Name]</h1>
-//         </section>
-//         <section className="favorites-section">
-//           <h2 className="section-title">Favorites</h2>
-//           <div className="lessons-container">
-//             {lessons.slice(0, 1).map((lesson, index) => (
-//               <Lesson key={index} {...lesson} />
-//             ))}
-//           </div>
-//         </section>
-//         <section className="recent-course-videos-section">
-//           <h2 className="section-title">Recent Course Videos</h2>
-//           <div className="lessons-container">
-//             {lessons.map((lesson, index) => (
-//               <Lesson key={index} {...lesson} />
-//             ))}
-//           </div>
-//         </section>
-//       </div>
-//     );
-//   };
-// export default LessonsDisplayArea;
