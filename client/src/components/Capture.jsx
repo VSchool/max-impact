04adc0a1fc8f96e85985b26c92270b6e101497bb
Clint Baker <clintjbaker@gmail.com>
2024-08-30 11:58:47 -0600

    offer 3 options to subscribe

--- client/src/components/Capture.jsx
+++ client/src/components/Capture.jsx
@@ -6,6 +6,8 @@ export default function Capture() {
   const [metadata, setMetadata] = useState({ customerId: '' })
   const [userInfo, setUserInfo] = useState('userId')
   const [productId, setProductId] = useState(import.meta.env.VITE_STRIPE_PRODUCT_ID)
+  const quarterlyId = import.meta.env.VITE_STRIPE_QUARTERLY_PRODUCT_ID
+  const yearlyId = import.meta.env.VITE_STRIPE_YEARLY_PRODUCT_ID
 
   useEffect(() => {
     if (user) {
@@ -16,13 +18,32 @@ export default function Capture() {
 
   return (
     <>
+    <h3>You need to subscribe to access the app.</h3>
+    <h5>Select an option below</h5>
       <div>
-        <h3>You need to subscribe</h3>
-        <div>Price is $35 / month</div>
+        <div>Monthly Subscription - $35 / month</div>
         <form action="/stripe/create-checkout-session" method="POST">
           <input type="hidden" id="customer" name="customer" value={userInfo.userId} />
           <input type="hidden" id="email" name="email" value={userInfo.email} />
           <input type="hidden" id="productId" name="productId" value={productId} />
+          <button type="submit">Checkout now!</button>
+        </form>
+      </div>
+      <div>
+        <div>Quarterly Subscription - $100 / 3 months (save $5)</div>
+        <form action="/stripe/create-checkout-session" method="POST">
+          <input type="hidden" id="customer" name="customer" value={userInfo.userId} />
+          <input type="hidden" id="email" name="email" value={userInfo.email} />
+          <input type="hidden" id="productId" name="productId" value={quarterlyId} />
+          <button type="submit">Checkout now!</button>
+        </form>
+      </div>
+      <div>
+        <div>Yearly Subscription - $350 / year (save $70)</div>
+        <form action="/stripe/create-checkout-session" method="POST">
+          <input type="hidden" id="customer" name="customer" value={userInfo.userId} />
+          <input type="hidden" id="email" name="email" value={userInfo.email} />
+          <input type="hidden" id="productId" name="productId" value={yearlyId} />
           <button type="submit">Checkout now!</button>
         </form>
       </div>
