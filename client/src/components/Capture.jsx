ab767309acfa261d2b83244fb00149b0ff0b863c
Clint Baker <clintjbaker@gmail.com>
2024-08-30 11:10:28 -0600

    when creating stripe checkout session, force the user to share email with stripe (auth0 & stripe email will always be the same)

--- client/src/components/Capture.jsx
+++ client/src/components/Capture.jsx
@@ -4,13 +4,13 @@ import { useAuth0 } from '@auth0/auth0-react'
 export default function Capture() {
   const { user } = useAuth0()
   const [metadata, setMetadata] = useState({ customerId: '' })
-  const [userId, setUserId] = useState('userId')
+  const [userInfo, setUserInfo] = useState('userId')
   const [productId, setProductId] = useState(import.meta.env.VITE_STRIPE_PRODUCT_ID)
 
   useEffect(() => {
     if (user) {
       setMetadata(user[`${import.meta.env.VITE_AUTH0_NAMESPACE}/app_metadata`])
-      setUserId(user.sub)
+      setUserInfo({userId: user.sub, email: user.email})
     }
   }, [user])
 
@@ -20,7 +20,8 @@ export default function Capture() {
         <h3>You need to subscribe</h3>
         <div>Price is $35 / month</div>
         <form action="/stripe/create-checkout-session" method="POST">
-          <input type="hidden" id="customer" name="customer" value={userId} />
+          <input type="hidden" id="customer" name="customer" value={userInfo.userId} />
+          <input type="hidden" id="email" name="email" value={userInfo.email} />
           <input type="hidden" id="productId" name="productId" value={productId} />
           <button type="submit">Checkout now!</button>
         </form>
